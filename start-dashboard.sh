e71c27e2d10531795029fcaddee30f2c6c287b9b
wuxian <wuxiangege@gmail.com>
2021-01-26 23:05:25 +0800

    仪表盘

--- start-dashboard.sh
+++ start-dashboard.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+#访问地址
 WEBSITE="0.0.0.0:2000"
+#域
 DOMAIN="com.baidu.api"
+#服务发现集群
 ETCD_CLUSTER_ADDS="172.19.76.101:2379,172.20.232.102:2379,172.16.93.103:2379"
 
+#启动
 nohup micro \
 --registry=etcdv3 \
 --registry_address=$ETCD_CLUSTER_ADDS web \
