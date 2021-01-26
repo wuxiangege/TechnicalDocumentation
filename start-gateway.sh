50a1b029668b1900eb558c6e65aed2962ef52847
wuxian <wuxiangege@gmail.com>
2021-01-26 23:02:58 +0800

    网关

--- start-gateway.sh
+++ start-gateway.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+#网关
 GATEWAY_ADDS="0.0.0.0:20000"
+#域
 DOMAIN="com.baidu.api"
 ETCD_CLUSTER_ADDS="172.19.76.101:2379,172.20.232.102:2379,172.16.93.103:2379"
 
+#启动
 nohup micro \
 --registry=etcdv3 \
 --registry_address=$ETCD_CLUSTER_ADDS \
